f1fa0cffbbead7ecfba57c65d6ce9a9eda79f82e
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2022-07-22 15:57:36 +0200

    First commit

---
+++ MoudlarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoudlarApp
+//
+//  Created by Dmitry Bespalov on 22.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
